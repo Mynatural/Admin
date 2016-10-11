91411e6e93849f10efcdc88737dcadc546789a84
Kunio SAWATANI <fsawa@fathens.org>
2016-10-11 08:25:41 +0000

    OnChanging と OnRemoving を実装

--- app/providers/model/lineup/lineup.ts
+++ app/providers/model/lineup/lineup.ts
@@ -29,8 +29,8 @@ export class LineupController {
     }
 
     async createNewKey(prefix: string, find: (v: string) => Promise<any>): Promise<string> {
-        var index = 0;
-        var key;
+        let index = 0;
+        let key;
         while (_.isNil(key) || !_.isNil(await find(key))) {
             key = `${prefix}_${index++}`;
         }
@@ -80,29 +80,29 @@ export class LineupController {
 const ROOT = "unauthorized";
 const LINEUP = "lineup";
 const SPEC = "_spec";
+const SPEC_KEY_PREFIX = "spec#";
+const IMAGES = "images";
 const INFO_JSON = "info.json.encoded";
 
 const SIDES: Info.SpecSide[] = ["FRONT", "BACK"];
 
 class Path {
     private static join(...list): string {
-        return _.join(_.flattenDeep(list), "/");
+        return _.join(_.filter(_.flattenDeep(list)), "/");
     }
 
     static itemDir(key: string): string {
         return Path.join(ROOT, LINEUP, key);
     }
 
-    static dirItem(o: Item): string {
-        return Path.itemDir(o.key);
+    static dirItem(o: Item, ...keys): string {
+        return Path.join(Path.itemDir(o.key), keys);
     }
 
-    static dirSpec(spec: Spec): string {
-        const list = [spec.isGlobal ? ROOT : Path.dirItem(spec.specGroup.item)];
-        list.push(SPEC);
-        if (!spec.isGlobal) list.push(spec.specGroup.key);
-        list.push(spec.key);
-        return Path.join(list);
+    static dirSpec(spec: Spec, ...keys): string {
+        const base = spec.isGlobal ? ROOT : Path.dirItem(spec.specGroup.item);
+        const group = spec.isGlobal ? null : spec.specGroup.key;
+        return Path.join(base, SPEC, group, spec.key, keys);
     }
 
     static infoItem(key: string): string {
@@ -114,31 +114,29 @@ class Path {
     }
 
     static imagesItemTitle(o: Item): string[] {
-        return [Path.join(Path.dirItem(o), "title.png")];
+        return [Path.dirItem(o, "title.png")];
     }
 
     static imagesItem(o: Item, side: Info.SpecSide): string[] {
         const names = _.map(o.specGroups, (specGroup) => {
             const spec = specGroup.current;
             const keys = _.map(spec.derivGroups, (v) => v.current.key);
-            keys.unshift(spec.key);
-            return keys;
+            return [Path.keyOfSpecImageItem(spec), keys];
         });
         return [Path.makeImageItem(o, side, names)];
     }
 
     static imagesDeriv(o: Deriv): string[] {
         const spec = o.derivGroup.spec;
-        return Path.illustrations(Path.dirSpec(spec), "derives", o.derivGroup.key, o.key);
+        return Path.illustrations(Path.dirSpec(spec, "derives", o.derivGroup.key, o.key));
     }
 
-    static imagesSpec(spec: Spec): string[] {
-        const keys = _.map(spec.derivGroups, (v) => v.current.key);
-        return Path.illustrations(Path.dirSpec(spec), "images", keys);
+    static imagesSpec(o: Spec): string[] {
+        return Path.makeImagesSpec(o, _.map(o.derivGroups, (v) => v.current.key));
     }
 
     static imagesMeasure(o: Measure): string[] {
-        return Path.illustrations(Path.dirItem(o.item), "measurements", o.key);
+        return Path.illustrations(Path.dirItem(o.item, "measurements", o.key));
     }
 
     private static illustrations(...paths): string[] {
@@ -146,11 +144,15 @@ class Path {
     }
 
     private static makeImageItem(o: Item, side: Info.SpecSide, keys): string {
-        return Path.join(Path.dirItem(o), "images", keys, `${side}.png`);
+        return Path.dirItem(o, IMAGES, keys, `${side}.png`);
     }
 
     private static makeImagesSpec(o: Spec, keys: string[]): string[] {
-        return Path.illustrations(Path.dirSpec(o), "images", keys);
+        return Path.illustrations(Path.dirSpec(o, IMAGES, keys));
+    }
+
+    static keyOfSpecImageItem(spec: Spec): string {
+        return `${SPEC_KEY_PREFIX}${spec.key}`
     }
 
     static allImagesItem(o: Item): string[] {
@@ -169,7 +171,7 @@ class Path {
         const lists = _.map(o.specGroups, (specGroup) =>
             _.flatMap(specGroup.availables, (spec) =>
                 _.map(Path.allKeysSpec(spec), (b) =>
-                    _.flatten([spec.key, b])
+                    _.flatten([Path.keyOfSpecImageItem(spec), b])
                 )
             )
         );
@@ -180,7 +182,7 @@ class Path {
         const lists = _.map(spec.derivGroups, (derivGroup) =>
             _.map(derivGroup.availables, (deriv) => deriv.key)
         );
-        return combinations(lists);
+        return _.isEmpty(lists) ? [[]] : combinations(lists);
     }
 }
 
@@ -276,28 +278,29 @@ async function refresh(item: Item): Promise<void> {
 class OnChanging {
     constructor(private s3: S3File) { }
 
-    private async moveFile(src, dst): Promise<void> {
-        if (!_.isEqual(src, dst) && await this.s3.exists(src)) {
-            await this.s3.move(src, dst);
-        }
-    }
-
-    private async moveDir(src, dst): Promise<void> {
-        if (!_.isEqual(src, dst) && await this.s3.exists(src)) {
-            await this.s3.moveDir(src, dst);
-        }
-    }
-
-    private async moveFiles(srcList, dstList): Promise<void> {
+    private async moveFiles(srcList: string[], dstList: string[]): Promise<void> {
         logger.debug(() => `Moving files: ${JSON.stringify(srcList, null, 4)}`);
         await Promise.all(_.map(_.zip(srcList, dstList),
-            (pair) => this.moveFile(pair[0], pair[1])
+            async (pair) => {
+                const src = pair[0];
+                const dst = pair[1];
+                if (!_.isEqual(src, dst) && await this.s3.exists(src)) {
+                    await this.s3.move(src, dst);
+                }
+            }
         ));
     }
 
-    private async moveDirs(srcList, dstList): Promise<void> {
+    private async moveDirs(srcList: string[], dstList: string[]): Promise<void> {
+        logger.debug(() => `Moving dirs: ${JSON.stringify(srcList, null, 4)}`);
         await Promise.all(_.map(_.zip(srcList, dstList),
-            (pair) => this.moveDir(pair[0], pair[1])
+            async (pair) => {
+                const src = pair[0];
+                const dst = pair[1];
+                if (!_.isEqual(src, dst)) {
+                    await this.s3.moveDir(src, dst);
+                }
+            }
         ));
     }
 
@@ -306,7 +309,7 @@ class OnChanging {
         await go();
         const dst = Path.dirItem(o);
 
-        await this.moveDir(src, dst);
+        await this.moveDirs([src], [dst]);
         await refresh(o);
     }
 
@@ -330,7 +333,7 @@ class OnChanging {
 
         await Promise.all([
             this.moveFiles(srcList, dstList),
-            this.moveDir(srcDir, dstDir)
+            this.moveDirs([srcDir], [dstDir])
         ]);
         await refresh(o.specGroup.item);
     }
@@ -340,7 +343,7 @@ class OnChanging {
         await go();
         const dstDir = Path.dirSpec(o);
 
-        await this.moveDir(srcDir, dstDir);
+        await this.moveDirs([srcDir], [dstDir]);
         await refresh(o.specGroup.item);
     }
 
@@ -354,19 +357,15 @@ class OnChanging {
     }
 
     async derivKey(o: Deriv, go: DoThru) {
-        const srcList = _.flatten([
+        const listup = () => _.flatten([
             Path.allImagesItem(o.derivGroup.spec.specGroup.item),
             Path.allImagesSpec(o.derivGroup.spec),
             Path.imagesDeriv(o)
         ]);
 
+        const srcList = listup();
         await go();
-
-        const dstList = _.flatten([
-            Path.allImagesItem(o.derivGroup.spec.specGroup.item),
-            Path.allImagesSpec(o.derivGroup.spec),
-            Path.imagesDeriv(o)
-        ]);
+        const dstList = listup();
 
         await this.moveFiles(srcList, dstList);
         await refresh(o.derivGroup.spec.specGroup.item);
@@ -385,38 +384,97 @@ class OnChanging {
 class OnRemoving {
     constructor(private s3: S3File) { }
 
+    async removeFiles(list: string[]): Promise<void> {
+        logger.debug(() => `Removing files: ${JSON.stringify(list, null, 4)}`);
+        await Promise.all(_.map(list, (file) => this.s3.remove(file)));
+    }
+
+    async removeDirs(list: string[]): Promise<void> {
+        logger.debug(() => `Removing dirs: ${JSON.stringify(list, null, 4)}`);
+        await Promise.all(_.map(list, (dir) => this.s3.removeDir(dir)));
+    }
+
+    private filesOfSpec(spec: Spec): string[] {
+        const item = spec.specGroup.item;
+        const key = Path.keyOfSpecImageItem(spec);
+        const base = Path.dirItem(item, IMAGES);
+        return _.filter(Path.allImagesItem(item), (path) => {
+            const names = _.filter(_.split(path.substr(base.length), "/"));
+            return _.some(names, (name) => _.isEqual(name, key));
+        });
+    }
+
+    private filesOfDeriv(deriv: Deriv): string[] {
+        const spec = deriv.derivGroup.spec;
+        const item = spec.specGroup.item;
+        const index = _.findIndex(spec.derivGroups, (dg) => _.isEqual(dg.key, deriv.derivGroup.key));
+
+        const specKey = Path.keyOfSpecImageItem(spec);
+        const itemBase = Path.dirItem(item, IMAGES);
+        const itemImages = _.filter(Path.allImagesItem(item), (path) => {
+            const names = _.filter(_.split(path.substr(itemBase.length), "/"));
+            const parts = _.takeWhile(_.tail(_.dropWhile(names,
+                (name) => !_.isEqual(name, specKey))),
+                (name) => !name.startsWith(SPEC_KEY_PREFIX)
+            );
+            return _.isEqual(parts[index], deriv.key);
+        });
+
+        const specBase = Path.dirSpec(spec, IMAGES);
+        const specImages = _.filter(Path.allImagesSpec(spec), (path) => {
+            const names = _.filter(_.split(path.substr(specBase.length), "/"));
+            return _.isEqual(names[index], deriv.key);
+        });
+
+        return itemImages.concat(specImages).concat(Path.imagesDeriv(deriv));
+    }
+
     async itemValue(o: Item, go: DoThru) {
         await go();
         await this.s3.removeDir(Path.dirItem(o));
     }
 
     async specGroup(o: SpecGroup, go: DoThru) {
+        const files = _.flatMap(o.availables, (spec) => this.filesOfSpec(spec));
+        const dirs = _.map(_.filter(o.availables, {isGlobal: false}),
+            (spec) => Path.dirSpec(spec)
+        );
         await go();
 
+        await Promise.all([this.removeDirs(dirs), this.removeFiles(files)]);
         await refresh(o.item);
     }
 
     async spec(o: Spec, go: DoThru) {
+        const files = this.filesOfSpec(o);
+        const dir = Path.dirSpec(o);
         await go();
 
+        await Promise.all([this.s3.removeDir(dir), this.removeFiles(files)]);
         await refresh(o.specGroup.item);
     }
 
     async derivGroup(o: DerivGroup, go: DoThru) {
+        const files = _.flatMap(o.availables, (d) => this.filesOfDeriv(d));
         await go();
 
+        await this.removeFiles(files);
         await refresh(o.spec.specGroup.item);
     }
 
     async deriv(o: Deriv, go: DoThru) {
+        const files = this.filesOfDeriv(o);
         await go();
 
+        await this.removeFiles(files);
         await refresh(o.derivGroup.spec.specGroup.item);
     }
 
     async measure(o: Measure, go: DoThru) {
+        const files = Path.imagesMeasure(o);
         await go();
 
+        await this.removeFiles(files);
         await refresh(o.item);
     }
 }
