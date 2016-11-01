c5e3ade9b289ab8ca2515390db1bf7124b40f450
Kunio SAWATANI <fsawa@fathens.org>
2016-11-01 11:19:35 +0000

    settings.js を作成
    gulpfile があると ionic が Gulp でビルドしようとするので、改名しました。

---
+++ settings.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+    fs = require('fs'),
+    _ = require('lodash');
+
+const target = './www/settings.json';
+const src = require(target);
+console.log("Settings SRC: " + JSON.stringify(src, null, 4));
+const dst = _.mapValues(src, (line) => {
+    const cmd = _.template(line);
+    return cmd(process.env);
+});
+console.log("Settings DST: " + JSON.stringify(dst, null, 4));
+fs.writeFileSync(target, JSON.stringify(dst));
